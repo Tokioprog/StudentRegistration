be1fb1b6b796eb229a8934298bca24220aaee61d
Roberto <master495@outlook.es>
2025-06-10 14:01:07 -0600

    codificacion de formulario MainForm

--- src/main/java/esfe/presentacion/users/MainForm.java
+++ src/main/java/esfe/presentacion/users/MainForm.java
@@ -1,6 +1,5 @@
 package esfe.presentacion.users;
 import esfe.dominio.User;
-
 import javax.swing.*;
 
 public class MainForm extends JFrame {
@@ -12,5 +11,51 @@ public class MainForm extends JFrame {
 
     public void setUserAutenticate(User userAutenticate) {
         this.userAutenticate = userAutenticate;
+    }
+
+    public MainForm() {
+        setTitle("Sistema en java de escritorio");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        createMenu();
+    }
+
+    private void createMenu() {
+        JMenuBar menuBar = new JMenuBar(); // Crea una nueva barra de menú.
+        setJMenuBar(menuBar); // Establece la barra de menú creada como la barra de menú de este JFrame (MainForm).
+
+        JMenu menuPerfil = new JMenu("Perfil"); // Crea un nuevo menú llamado "Perfil".
+        menuBar.add(menuPerfil); // Agrega el menú "Perfil" a la barra de menú.
+
+        JMenuItem itemChangePassword = new JMenuItem("Cambiar contraseña");
+        menuPerfil.add(itemChangePassword);
+        itemChangePassword.addActionListener(e -> {
+            ChangePasswordForm changePassword = new ChangePasswordForm(this);
+            changePassword.setVisible(true);
+
+        });
+
+        JMenuItem itemChangeUser = new JMenuItem("Cambiar de usuario");
+        menuPerfil.add(itemChangeUser);
+        itemChangeUser.addActionListener(e -> {
+            LoginForm loginForm = new LoginForm(this);
+            loginForm.setVisible(true);
+        });
+
+        JMenuItem itemSalir = new JMenuItem("Salir");
+        menuPerfil.add(itemSalir);
+        itemSalir.addActionListener(e -> System.exit(0));
+
+        // Menú "Matenimiento"
+        JMenu menuMantenimiento = new JMenu("Mantenimientos");
+        menuBar.add(menuMantenimiento);
+
+        JMenuItem itemUsers = new JMenuItem("Usuarios");
+        menuMantenimiento.add(itemUsers);
+        itemUsers.addActionListener(e -> {
+            UserReadingForm userReadingForm = new UserReadingForm(this);
+            userReadingForm.setVisible(true);
+        });
     }
 }
